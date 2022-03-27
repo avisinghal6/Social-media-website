f1ac40e0b34a6dabd3b8ce134755df57de9342ba
avi singhal <97785770+avisinghal6@users.noreply.github.com>
2022-03-27 21:07:43 +0530

    new cookie created because flash object is added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const sassMiddleware= require('node-sass-middleware');
 app.use(sassMiddleware({
     src:'./assets/scss', //usng "." makes it work
     dest:'./assets/css',
-    debug:true,
+    debug:false,
     outputStyle: 'extended',
     prefix:'/css'
 }));
